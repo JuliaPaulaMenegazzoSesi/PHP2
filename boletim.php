de62e63e5e435e00b671a11ec829a27cd9f739b4
JuliaPaulaMenegazzoSesi <159256141+JuliaPaulaMenegazzoSesi@users.noreply.github.com>
2024-10-01 12:27:40 -0300

    Update boletim.php

--- boletim.php
+++ boletim.php
@@ -8,17 +8,16 @@
 <body>
     <h1>Boletim</h1>
     <form name="boletim" action="" method="get">
-        <input type="number" id="n1">
-        <input type="number" id="n2">
-        <input type="number" id="n3">
-        <input type="number" id="n4">
-        <input type="number" id="n5">
-        <button>Enviar</button>
-    </form>      
-    <h2 id="resultado"></h2>
-</body>
-</html>
-<?php 
+        <input type="number" name="n1" required>
+        <input type="number" name="n2" required>
+        <input type="number" name="n3" required>
+        <input type="number" name="n4" required>
+        <input type="number" name="n5" required>
+        <button type="submit">Enviar</button>
+    </form> 
+
+
+    <?php 
     if (isset($_GET['n1'],
               $_GET['n2'],
               $_GET['n3'],
@@ -33,5 +32,27 @@
 
         $media = ($n1 + $n2 + $n3 + $n4 + $n5) / 5;
 
+        if($media > 89) {
+            echo "<h2>Média: " . number_format($media, 2, ',', '.') . "</h2>";
+            echo "<h2>Nota A</h2>";
+        } elseif ($media > 79) {
+            echo "<h2>Média: " . number_format($media, 2, ',', '.') . "</h2>";
+            echo "<h2>Nota B</h2>";
+        } elseif ($media > 69) {
+            echo "<h2>Média: " . number_format($media, 2, ',', '.') . "</h2>";
+            echo "<h2>Nota C</h2>";
+        } elseif ($media > 59) {
+            echo "<h2>Média: " . number_format($media, 2, ',', '.') . "</h2>";
+            echo "<h2>Nota D</h2>";
+        } else {
+            echo "<h2>Média: " . number_format($media, 2, ',', '.') . "</h2>";
+            echo "<h2>Nota F</h2>";
+        }
+
     }
 ?>
+
+
+   
+</body>
+</html>
